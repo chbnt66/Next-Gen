e82fafe4b280c6348cd1d307f9d127627433c514
chbnt66 <charles.brunet2002@gmail.com>
2025-11-22 15:53:37 +0100

    Modify notebook name for testing MistralAI

--- .nextgen_venv/bin/activate.fish
+++ .nextgen_venv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV /Users/charles.brunet/Next-Gen/.nextgen_venv
+set -gx VIRTUAL_ENV /Users/charles.brunet/Desktop/Next-Gen/.nextgen_venv
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/"bin $PATH
